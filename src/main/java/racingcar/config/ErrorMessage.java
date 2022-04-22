8e122380260ea8f13a88a59d48e3e6a62d4941f5
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-22 20:53:36 +0900

    feat: ErrorMessage 추가
    범위제한 메시지 추가

--- src/main/java/racingcar/config/ErrorMessage.java
+++ src/main/java/racingcar/config/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     SameName("각각 다른 이름을 입력해주세요."),
     EmptyName("이름이 비어있습니다."),
     ExceedCount("5글자 이하만 입력해주세요."),
-    NotNumber("정수 숫자만 입력해주세요");
+    NotNumber("정수 숫자만 입력해주세요"),
+    ExceedNumber("2,147,483,647 이하의 양수만 입력해주세요.");
 
     private final String message;
 
