3df692b406dac5f727591410af8b4038c3110be8
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-24 21:57:10 +0900

    refactor: 화면 문구 수정

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public class View {
-    private static final String INPUT_CAR_NAME = "경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_COUNT = "시도할 회수는 몇회인가요?";
     private static final String PLAY_RESULT = "실행 결과";
     private static final String WINNER_SUBTITLE = "최종 우승자: ";
